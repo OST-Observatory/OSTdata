a7d229cb4d72f8b88efa31fa786741dcb9f5a29e
ostlight <rhainich@astro.physik.uni-potsdam.de>
2023-06-01 16:53:30 +0200

    update file type analysis

--- analyze_exposure_type.py
+++ analyze_exposure_type.py
@@ -6,6 +6,9 @@ from pathlib import Path
 from astropy.io import fits
 from astropy.visualization import simple_norm
 
+from scipy.ndimage import median_filter, maximum_filter
+from scipy.interpolate import InterpolatedUnivariateSpline
+
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -31,10 +34,26 @@ if __name__ == "__main__":
                 header = fits.getheader(file_path, 0)
 
                 imagetyp = header.get('IMAGETYP', 'UK')
+                if imagetyp == 'Flat Field':
+                    imagetyp = 'flat'
+                if imagetyp == 'Dark Frame':
+                    imagetyp = 'dark'
+                if imagetyp == 'Light Frame':
+                    imagetyp = 'light'
+                if imagetyp == 'Bias Frame':
+                    imagetyp = 'bias'
                 objectname = header.get('OBJECT', 'UK')
                 exptime = header.get('EXPTIME', -1)
 
                 image_data_original = fits.getdata(file_path, 0)
+
+                img_shape = image_data_original.shape
+                # print(image_data_original.shape)
+                # print(image_data_original.ndim)
+
+                if image_data_original.ndim > 2:
+                    continue
+
                 image_data = ndimage.median_filter(image_data_original, size=10)
 
                 if figure:
@@ -58,6 +77,7 @@ if __name__ == "__main__":
                 n_non_zero_histo = len(np.nonzero(histogram)[0])
 
                 median = np.median(image_data)
+                mean = np.mean(image_data)
                 variance = np.var(image_data)
                 standard = np.std(image_data)
 
@@ -94,8 +114,10 @@ if __name__ == "__main__":
                 dados_spectra = signal.find_peaks(
                     y_sum-y_sum_min,
                     height=y_sum_median,
+                    # height=y_sum_mean,
                     width=(132, 139),
                     rel_height=0.9,
+                    prominence=30000.,
                     )
                 print(dados_spectra)
                 if dados_spectra[0].size > 1:
@@ -109,11 +131,124 @@ if __name__ == "__main__":
                     width=(16, 50),
                     # rel_height=0.9,
                     )
-                    if baches_spectra[0].size >= 4:
-                        spectra_type = 'baches'
-                        # print(baches_spectra)
 
-                    print(baches_spectra)
+                    n_order = baches_spectra[0].size
+
+                    if n_order >= 4:
+                        spectra_type = 'baches'
+                        print(baches_spectra)
+                        # print(baches_spectra[-1])
+                        # print(baches_spectra[-1]['right_ips'])
+
+                        x_sum_order = 0.
+                        n_pixel_in_orders = 0
+                        for i in range(0,n_order):
+                            start_order = int(baches_spectra[-1]['left_ips'][i])
+                            end_order = int(baches_spectra[-1]['right_ips'][i])
+
+                            x_sum_order += np.sum(
+                                image_data_original[start_order:end_order,:]
+                                )
+                            n_pixel_in_orders += (end_order - start_order) \
+                                                 * img_shape[1]
+
+                        flux_in_orders_average = x_sum_order / n_pixel_in_orders
+                        print(flux_in_orders_average, n_pixel_in_orders)
+
+                        # variance, standard = 0., 0.
+                        # for i in range(0,n_order):
+                        #     start_order = int(baches_spectra[-1]['left_ips'][i])
+                        #     end_order = int(baches_spectra[-1]['right_ips'][i])
+                        #
+                        #     x_sum_order = np.sum(
+                        #         image_data_original[start_order:end_order,:],
+                        #         axis=0,
+                        #         )
+                        #     # x_sum_order_index_max = np.argmax(x_sum_order)
+                        #
+                        #     x_sum_order_median_filtered = median_filter(
+                        #         x_sum_order,
+                        #         size=40,
+                        #         )
+                        #     x_sum_order_median_max_filtered = maximum_filter(
+                        #         x_sum_order_median_filtered,
+                        #         size=40,
+                        #         )
+                        #
+                        #     norm_fit = InterpolatedUnivariateSpline(
+                        #         np.arange(len(x_sum_order_median_max_filtered)),
+                        #         x_sum_order_median_max_filtered,
+                        #         k=2,
+                        #         )
+                        #
+                        #     #   Normalize spectrum
+                        #     x_data = np.arange(len(x_sum_order))
+                        #     cont = norm_fit(x_data)
+                        #     x_sum_order_norm = x_sum_order / cont
+                        #
+                        #     mask = np.argwhere(x_sum_order_norm > 1.)
+                        #     x_sum_order_norm[mask] = 1.
+                        #
+                        #     variance += np.var(x_sum_order_norm)
+                        #     standard += np.std(x_sum_order_norm)
+                        #
+                        # print(
+                        #     variance/n_order,
+                        #     standard/n_order,
+                        #     )
+
+                        # start_order = int(baches_spectra[-1]['left_ips'][2])
+                        # end_order = int(baches_spectra[-1]['right_ips'][2])
+                        #
+                        # x_sum_order = np.sum(
+                        #     image_data_original[start_order:end_order,:],
+                        #     axis=0,
+                        #     )
+                        # # x_sum_order_index_max = np.argmax(x_sum_order)
+                        #
+                        # x_sum_order_median_filtered = median_filter(
+                        #     x_sum_order,
+                        #     size=40,
+                        #     )
+                        # x_sum_order_median_max_filtered = maximum_filter(
+                        #     x_sum_order_median_filtered,
+                        #     size=40,
+                        #     )
+                        #
+                        # norm_fit = InterpolatedUnivariateSpline(
+                        #     np.arange(len(x_sum_order_median_max_filtered)),
+                        #     x_sum_order_median_max_filtered,
+                        #     k=2,
+                        #     )
+                        #
+                        # #   Normalize spectrum
+                        # x_data = np.arange(len(x_sum_order))
+                        # cont = norm_fit(x_data)
+                        # x_sum_order_norm = x_sum_order / cont
+                        #
+                        # mask = np.argwhere(x_sum_order_norm > 1.)
+                        # x_sum_order_norm[mask] = 1.
+                        #
+                        # print(
+                        #     np.mean(x_sum_order_norm),
+                        #     np.median(x_sum_order_norm),
+                        #     np.var(x_sum_order_norm),
+                        #     np.std(x_sum_order_norm),
+                        #     )
+                        #
+                        # fig = plt.figure(figsize=(20, 7))
+                        # ax1 = plt.subplot(1, 2, 1)
+                        # ax2 = plt.subplot(1, 2, 2)
+                        #
+                        # ax1.plot(x_data, x_sum_order)
+                        # ax1.plot(x_data, cont)
+                        #
+                        # ax2.plot(x_data, x_sum_order_norm)
+                        #
+                        # plt.show()
+                        # plt.close()
+
+                    # print(baches_spectra)
                     # print(
                     #     signal.find_peaks(
                     #         y_sum-y_sum_min,
@@ -125,17 +260,17 @@ if __name__ == "__main__":
 
                 img_type_estimate = 'UK'
 
-                if median == 65535.:
+                if int(median) == 65535:
                     img_type_estimate = 'over_exposed'
 
                 if (n_non_zero_histo <= 2 and
                     standard < 15 and
                     spectra_type is None):
                     # print('exptime', exptime)
-                    if exptime > 0:
+                    if exptime <= 0.01:
+                        img_type_estimate = 'bias'
+                    else:
                         img_type_estimate = 'dark'
-                        if exptime <= 0.01:
-                            img_type_estimate = 'bias'
 
                 # if (n_non_zero_histo >= 40 and
                 elif (n_non_zero_histo >= 100 and
@@ -156,7 +291,7 @@ if __name__ == "__main__":
                     # standard > 1300):
                 elif (n_non_zero_histo > 60 and
                     spectra_type == 'baches'):
-                    if median > 4000:
+                    if median > 4000 or flux_in_orders_average > 1700:
                         # max_histo_id == 7 and
                         img_type_estimate = 'flat'
                     else:
@@ -232,7 +367,7 @@ if __name__ == "__main__":
                   imagetyp,
                   '\tEstimated: ',
                   img_type_estimate,
-                  '\tSpectral type: ',
+                  '\tInstrument: ',
                   spectra_type,
                   )
                 # print(histogram)
@@ -247,6 +382,8 @@ if __name__ == "__main__":
                 print(
                   'Median = ',
                   median,
+                  '\tMean = ',
+                  mean,
                   '\tVariance = ',
                   variance,
                   '\tStandard deviation = ',
@@ -254,12 +391,14 @@ if __name__ == "__main__":
                   )
                 print()
 
-                outfile.write(
-                    f'Image type: {imagetyp}\tEstimated: {img_type_estimate}\n'
-                    )
-                outfile.write(
-                    f'File: {file_path.absolute()}\n\n'
-                    )
+                if ((img_type_estimate != imagetyp or imagetyp == 'light') and
+                    not (img_type_estimate == 'light' and imagetyp == 'wave')):
+                    outfile.write(
+                        f'Image type: {imagetyp}\tEstimated: {img_type_estimate}\tInstrument: {spectra_type}\n'
+                        )
+                    outfile.write(
+                        f'File: {file_path.absolute()}\n\n'
+                        )
 
         print('---------------------------------------------')
         print()
@@ -268,7 +407,6 @@ if __name__ == "__main__":
 #   Model parameter:
 #       instrument: baches or dados --> spectra_type
 #       derived_exposure_type = bias, dark, wave, flat, light, over_exposed
-# # # #       spec_file_type: wave, flat, light  --> or extend image type with 'wave'
-#
+#       add flag if derived_exposure_type and Header value are consistent
 
     outfile.close()
