d638f2649adcefda9d1a54514dc942d6cc390001
ostlight <rhainich@astro.physik.uni-potsdam.de>
2023-09-26 13:28:42 +0200

    small update

--- estimate_exposure_type_classic.py
+++ estimate_exposure_type_classic.py
@@ -13,36 +13,44 @@ from obs_run.models import DataFile
 
 if __name__ == "__main__":
     for data_file in DataFile.objects.all():
-        # print(data_file.datafile)
-        file_path = Path(data_file.datafile)
+        #   Check if the file was already analyzed
+        if data_file.status_parameters == 'CLA':
+            continue
 
-        status, _, estimated_exposure_type, spectrum_type, _ = analyze_and_update_exposure_type(
-            file_path,
-            plot_histogram=False,
-            print_to_terminal=True,
-        )
-        print(data_file.datafile)
-        print(status, estimated_exposure_type, spectrum_type)
-        print()
+        try:
+            # print(data_file.datafile)
+            file_path = Path(data_file.datafile)
 
-        if status:
-            if estimated_exposure_type == 'bias':
-                data_file.exposure_type = 'BI'
-            if estimated_exposure_type == 'dark':
-                data_file.exposure_type = 'DA'
-            if estimated_exposure_type == 'flat':
-                data_file.exposure_type = 'FL'
-            if estimated_exposure_type == 'light':
-                data_file.exposure_type = 'LI'
+            status, _, estimated_exposure_type, spectrum_type, _ = analyze_and_update_exposure_type(
+                file_path,
+                plot_histogram=False,
+                print_to_terminal=True,
+            )
+            print(data_file.datafile)
+            print(status, estimated_exposure_type, spectrum_type)
+            print()
 
-            if spectrum_type == 'dados':
-                data_file.spectrograph = 'D'
-            if spectrum_type == 'baches':
-                data_file.spectrograph = 'B'
-            if spectrum_type == 'einstein':
-                data_file.spectrograph = 'E'
+            if status:
+                if estimated_exposure_type == 'bias':
+                    data_file.exposure_type = 'BI'
+                if estimated_exposure_type == 'dark':
+                    data_file.exposure_type = 'DA'
+                if estimated_exposure_type == 'flat':
+                    data_file.exposure_type = 'FL'
+                if estimated_exposure_type == 'light':
+                    data_file.exposure_type = 'LI'
 
-            data_file.status_parameters = 'CLA'
+                if spectrum_type == 'dados':
+                    data_file.spectrograph = 'D'
+                if spectrum_type == 'baches':
+                    data_file.spectrograph = 'B'
+                if spectrum_type == 'einstein':
+                    data_file.spectrograph = 'E'
 
-            data_file.save()
+                data_file.status_parameters = 'CLA'
+
+                data_file.save()
+
+        except:
+            print(f"{data_file.datafile} could not be analyzed")
 
